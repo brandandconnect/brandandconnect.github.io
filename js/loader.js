9c149c05024d47f94adffb5571e3e0cdf8f05e43
cristianlunarl <cristian_luna00@hotmail.com>
2022-06-28 16:57:22 -0500

    improvements

--- js/loader.js
+++ js/loader.js
@@ -2,7 +2,6 @@ content = setTimeout (function loading() {
     const loader = document.getElementById('loader')
     loader.style.visibility='hidden'
     loader.style.opacity=0
-    }, 1000
-)
+    }, 1000)
 
 window.onload = content
